3a1aa77baeed2539a109311dff904da86ff1e6f4
Mariam_Mohamed_Rabie <234274800+Mariam-Mohamed-Rabie@users.noreply.github.com>
2025-11-05 00:20:28 +0200

    Update PlayerGUI.cpp

--- PlayerGUI.cpp
+++ PlayerGUI.cpp
@@ -108,6 +108,8 @@ void PlayerGUI::resized()
     goToStartButton.setBounds(540, y, 100, 40);
     goToEndButton.setBounds(640, y, 100, 40);
     play_pauseButton.setBounds(780, y, 100, 40);
+	prevButton.setBounds(900, y, 80, 40);
+	nextButton.setBounds(990, y, 80, 40);
     setAButton.setBounds(20, 250, 100, 40);
     setBButton.setBounds(140, 250, 100, 40);
 
@@ -116,6 +118,8 @@ void PlayerGUI::resized()
 
     volumeSlider.setBounds(20, 100, getWidth() - 40, 30);
     positionSlider.setBounds(20, 200, getWidth() - 40, 30);
+	fileInfoLabel.setBounds(20, 150, getWidth() - 40, 40);
+	playListComboBox.setBounds(20, 200, getWidth() -40, 50);
 	speedSlider.setBounds(20, 150, getWidth() - 40, 30);
     timeLabel.setBounds(20, 180, getWidth() - 40, 20);
 }
@@ -136,14 +140,77 @@ void PlayerGUI::buttonClicked(juce::Button* button)
             "*.wav;*.mp3");
 
         fileChooser->launchAsync(
-            juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
+            juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectMultipleItems,
             [this](const juce::FileChooser& fc)
             {
-                auto file = fc.getResult();
-                if (file.existsAsFile())
-                {
-					playerAudio.loadFile(file);
-                }
+                auto results = fc.getResults();
+				
+				if (results.isEmpty())
+				{
+					fileInfoLabel.setText("No File Selected OR File not Found.", juce::dontSendNotification);
+					return;
+				}
+
+				playListFiles.clear();
+				for (auto& f : results)
+				{
+				    playListFiles.add(f);
+				}
+
+				playListComboBox.clear();
+				for (int i = 0; i < playListFiles.size(); ++i)
+				{
+				    playListComboBox.addItem(playListFiles[i].getFileName(), i + 1); 
+				}
+				if (playListFiles.size() > 0)
+				{
+				    currentFileIndex = 0;
+				    playListComboBox.setSelectedId(1);
+				
+					bool loaded = playerAudio.loadFile(playListFiles[currentFileIndex]);
+				    if (loaded)
+				    {
+				        playerAudio.start();
+				        fileInfoLabel.setText(playListFiles[currentFileIndex].getFileName(), juce::dontSendNotification);
+				    }
+				    else {
+						fileInfoLabel.setText("Failed to load file: " + playListFiles[currentFileIndex].getFileName(), juce::dontSendNotification);
+				    }
+				}
+				// metadata display    
+				if (currentFileIndex >= 0 && currentFileIndex < playListFiles.size())
+				{
+				    juce::File file = playListFiles[currentFileIndex];
+				    juce::AudioFormatManager formatManager;
+				    formatManager.registerBasicFormats();
+				    auto reader = std::unique_ptr<juce::AudioFormatReader>(formatManager.createReaderFor(file));
+				    juce::String Info;
+				
+				    if (reader != nullptr)
+				    {
+				        auto meta = reader->metadataValues;
+				
+				        juce::String title = meta.getValue("title", meta.getValue("Title", "Unknown Title!"));
+				        juce::String author = meta.getValue("artist",
+									          meta.getValue("Artist",
+								              meta.getValue("Author",
+				                    		  meta.getValue("Performer", "Unknown Author!"))));
+				        double lengthInSeconds = playerAudio.getLength();
+				
+				        if (meta.size() == 0 || (title == "Unknown Title!" && author == "Unknown Author!"))
+				        {
+				            Info = "File: " + file.getFileName() + " | Duration: " + juce::String(lengthInSeconds, 2) + " sec";
+				        }
+				        else {
+				            Info = "Title: " + title + "\nAuthor: " + author + "\nDuration: " + juce::String(lengthInSeconds, 2) + " sec";
+				        }
+				    }
+				    else {
+				        double lengthInSeconds = playerAudio.getLength();
+				        Info = "File: " + file.getFileName() + " | Duration: " + juce::String(lengthInSeconds, 2) + " sec";
+				    }
+				    fileInfoLabel.setText(Info, juce::dontSendNotification);
+				}
             });
     }
 
@@ -179,6 +246,20 @@ void PlayerGUI::buttonClicked(juce::Button* button)
     {
         playerAudio.goToEnd();
     }
+	if (button == &nextButton)
+	{
+		playerAudio.playNext(playListFiles, currentFileIndex);
+	    if (currentFileIndex >= 0 && currentFileIndex < playListFiles.size()) {
+	        fileInfoLabel.setText(playListFiles[currentFileIndex].getFileName(), juce::dontSendNotification);
+	    }
+	}
+	if (button == &prevButton)
+	{
+		playerAudio.playPrevious(playListFiles, currentFileIndex);
+	    if (currentFileIndex >= 0 && currentFileIndex < playListFiles.size()) {
+	        fileInfoLabel.setText(playListFiles[currentFileIndex].getFileName(), juce::dontSendNotification);
+	    }
+	}
     if (button == &setAButton)
     {
         pointA = playerAudio.getPosition();
@@ -191,6 +272,7 @@ void PlayerGUI::buttonClicked(juce::Button* button)
         abLoopEnabled = true;
     }
 }
+
 void PlayerGUI::sliderValueChanged(juce::Slider* slider) {
         if (slider == &volumeSlider)
             playerAudio.setGain((float)slider->getValue());
@@ -232,6 +314,7 @@ void PlayerGUI::timerCallback()
 
  repaint();
 }
+
 
 
 
