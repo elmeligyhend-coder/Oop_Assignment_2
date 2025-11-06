ec543246fc3b4b985ec79eb81d94cde42f0e46a8
maiMahmoud1 <101149936+maiMahmoud1@users.noreply.github.com>
2025-11-06 15:01:46 +0200

    Refactor PlayerAudio: Add loadFile2 and setGain2
    Added new methods for loading files and setting gain.

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -10,7 +10,8 @@ public:
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
-
+     bool loadFile2(const juce::File& file);
+ void setGain2(float gain);
     bool loadFile(const juce::File& file);
     void start();
     void stop();
@@ -26,7 +27,6 @@ public:
     void goToEnd();
 	void playNext(const juce::Array<juce::File>& playlistFiles, int& currentFileIndex);
 	void playPrevious(const juce::Array<juce::File>& playlistFiles, int& currentFileIndex);
-
     bool isMuted = false;
     float lastGain = 0.5f;
     void switcherMute();
@@ -37,6 +37,11 @@ private:
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
     std::unique_ptr<juce::ResamplingAudioSource> resamplingSource; 
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource2;
+    juce::AudioTransportSource transportSource2;
+    juce::MixerAudioSource mixer;
+    float gain1 = 1.0f; 
+    float gain2 = 1.0f; 
     juce::AudioThumbnailCache thumbnailCache{ 5 };  
     juce::AudioThumbnail thumbnail{ 512, formatManager, thumbnailCache };
     double playbackSpeed = 1.0;
@@ -44,6 +49,7 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
 };
+
 
 
 
